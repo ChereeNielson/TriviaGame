f49149a5dfdc832e081386b3a94ed1e54b42b1d6
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2018-12-01 11:17:20 -0700

    working timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,65 +4,65 @@ $(document).ready(function () {
         {
             question: "What is Indiana Jones' real first name?",
             choices: ["Indy", "Junior", "Henry", "Marcus"],
-            answer: 2
+            answer: "henry"
         }, 
 
         {
             question: "What actor played the villain in the 1994 movie <i>Speed</i>?",
             choices: ["Keanu Reeves", "Dennis Hopper", "Howard Payne", "Jeff Daniels"],
-            answer: 1
+            answer: "dennis hopper"
         },
 
         {
             question: "How many people were killed in the 1996 movie <i>Scream</i>?",
             choices: ["Seven", "Nine", "Six", "Five"],
-            answer: 0
+            answer: "seven"
         },
         
         {
             question: "Which of these \"I am Jack\" lines is NOT uttered by the Narrator in the 1999 movie <i>Fight Club</i>?",
             choices: ["\"I am Jack's broken heart.\"", "\"I am Jack's enflamed sense of rejection.\"", "\"I am Jack's wasted life.\"", "\"I am Jack's complete lack of remorse.\""],
-            answer: 3
+            answer: "remorse"
         },
 
         {
             question: "Who told Chunk to do the \"Truffle Shuffle\" in the 1985 movie <i>The Goonies</i>?",
             choices: ["Brand", "Mikey", "Data", "Mouth"],
-            answer: 3
+            answer: "mouth"
         },
 
         {
             question: "Which 80s movie was the highest grossing film of the decade?",
             choices: ["Return of the Jedi", "E.T. <i>The Extraterrestrial</i>", "Ghostbusters", "Batman"],
-            answer: 1
+            answer: "et"
         },
 
         {
             question: "Who is Keyser Soze in the movie <i>The Usual Suspects</i>?",
             choices: ["Dean Keaton", "Michael Mcmanus", "Roger \“Verbal\” Kint", "Fred Fenster"],
-            answer: 2
+            answer: "verbal"
         },
 
         {
             question: "In <i>Signs</i>, what does Mel Gibson tell Joaquin Phoenix when they’re confronting the alien?",
             choices: ["\"Swing away.\"", "\"RUN!\"", "\"Hold still. They don’t see you if you don’t move.\"", "\"Grab the gun.\""],
-            answer: 0
+            answer: "swing"
         }, 
 
         {
             question: "In <i>The Big Lebowski</i>, what ties the Dude's room together?",
             choices: ["the lamp", "the rug", "the ottoman", "the chair"],
-            answer: 1
+            answer: "rug"
         },
 
         {
             question: "What is Edward grilling at the neighborhood barbeque in the movie <i>Edward Scissorhands</i>?",
             choices: ["steak", "ribs", "shish kebobs", "burgers"],
-            answer: 2
+            answer: "kebobs"
         }
     ];
 
-    // Variables that connect to their $Counterparts
+    // Variables that connect to their $Counterparts //
     let timer = $("#timer");
     let startButton = $("#startButton");
 
@@ -76,91 +76,76 @@ $(document).ready(function () {
     let choice4 = $("#answer3");
     let next = $("#next");
     let back = $("back");
+    let correctAnswer = 0;
+    let wrongAnswer = 0;
+    let unanswered = 0;
 
 
-}
-
-$("#startBtn").click(function() {
-    if (isGameStarted === false) {
-      isGameStarted = true;
-      timer.show();
-      question.show();
-      choice1.show();
-      choice2.show();
-      choice3.show();
-      choice4.show();
-      startBtn.hide();
-      run();
-      populate();
-
-      if (seconds >= 1) {
-        console.log("the game is still going");
-      }
+    function beginTimer() {
+        let seconds = 121;
+        setInterval(function() {
+            seconds--;
+            if (seconds >= 0) {
+                var minutes = Math.floor(((seconds * 1000) % (1000 * 60 * 60)) / (1000 * 60));
+                var x = Math.floor(((seconds * 1000) % (1000 * 60)) / 1000);
+                $("#timeRemaining").html(minutes + ":" + x);
+            }
+            if (seconds == 0) {
+                clearInterval(seconds);
+                $("#triviaContainer").css("display", "none");
+                $("#results").css("display", "block");
+            }
+        }, 1000); 
+        
     }
 
+    function getResults() {
+        let userAnswer = [];
+        let selectedVal = "";
+        let question1 = $("input[type='radio'][name='first-name']:checked").val();
+            if(allQuestions[0].answer == question1) {
+                correctAnswer ++;
+            } else if (question1 == null) {
+                unanswered ++;
+            } else {
+                wrongAnswer ++;
+            };
+        let question2 = $("input[type='radio'][name='villian']:checked").val();
+            if(allQuestions[1].answer == question2) {
+                correctAnswer ++;
+            } else if (question2 == null) {
+                unanswered ++;
+            } else {
+                wrongAnswer ++;
+            }
+        let question3 = $("input[type='radio'][name='killed']:checked").val();
+            if(allQuestions[2].answer == question3) {
+                correctAnswer ++;
+            } else if (question3 == null) {
+                unanswered ++;
+            } else {
+                wrongAnswer ++;
+            }
+        let question4 = $("input[type='radio'][name='jack']:checked").val();
+        let question5 = $("input[type='radio'][name='chunk']:checked").val();
+        let question6 = $("input[type='radio'][name='grossing']:checked").val();
+        let question7 = $("input[type='radio'][name='keyser']:checked").val();
+        let question8 = $("input[type='radio'][name='signs']:checked").val();
+        let question9 = $("input[type='radio'][name='dude']:checked").val();
+        let question10 = $("input[type='radio'][name='edward']:checked").val();
+        allQuestions[0]
+        // print out results $("#timeRemaining").html(minutes + ":" + x); //
+    }
 
+    // Click start button to reveal trivia questions //
 
+    $("button#startButton").click(function() {
+        $("#triviaContainer").css("display", "block");
+        $("button#startButton").css("display", "none");
+        
+    // Create a 2 minute countdown timer //
 
-    let timeoutId = window.setTimeout(function() {
-
-    $("#start").click(stopwatch.start);
-  };
-
-  let timeRemaining;
-  let clockRunning = false;
-
-  let stopwatch = {
-        time: 0,
-        lap: 1,
-
-      reset: function() {
-        stopwatch.time = 0;
-        stopwatch.lap = 1;
-        $("#display").text("00:00");
-        $("#laps").text("");
-      },
-
-      start: function() {
-        if (!clockRunning) {
-            setInterval(stopwatch.start);
-            intervalId = setInterval(increment, 1000);
-        }
-      },
-
-      stop: function() {
-        clearInterval(stopwatch.stop);
-        clockRunning = false;
-      },
-
-      recordLap: function(){
-        let converted = stopwatch.timeConverter(stopwatch.time);
-        $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "<p>");
-        stopwatch.lap++;
-      },
-
-      count: function() {
-        stopwatch.time++;
-        let converted = stopwatch.timeConverter(stopwatch.time);
-        console.log(converted);
-        $("#display").text(converted);
-      },
-
-      timeConverter: function(t) {
-        let minutes = Math.floor(t / 60);
-        let seconds = t - (minutes * 60);
-    
-        if (seconds < 10) {
-          seconds = "0" + seconds;
-        }
-    
-        if (minutes === 0) {
-          minutes = "00";
-        }
-    
-        else if (minutes < 10) {
-          minutes = "0" + minutes;
-        }
-    
-        return minutes + ":" + seconds;
-      }
-};
+        beginTimer();
+          
+    });
+});
